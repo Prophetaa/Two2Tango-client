002f2af9f13464f0382e3b149fd26b971f0f28c1
DevJ5 <36604614+DevJ5@users.noreply.github.com>
2018-11-08 11:34:55 +0100

    Shows table on results page

--- src/components/resultsPage/Results.js
+++ src/components/resultsPage/Results.js
@@ -1,31 +1,97 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Results() {
+export default function Results(props) {
 	return (
 		<div>
-			<ul>
-				<li>
-					<div className="result-item">
-						<div className="card-body">
-							<h5 className="card-title">Dummy person</h5>
+			<table class="table table-striped">
+				<thead>
+					<tr>
+						<th scope="col" />
+						<th scope="col">Name</th>
+						<th scope="col">Role</th>
+						<th scope="col">Level</th>
+						<th scope="col">Age</th>
+						<th scope="col">Gender</th>
+						<th scope="col">Height</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th scope="row">
 							<img
 								src="images/dummy.jpg"
 								alt=""
 								width="100"
 								style={{ borderRadius: '50%' }}
 							/>
-							<p className="card-text">
-								With supporting text below as a natural lead-in to additional
-								content.
-							</p>
-							<Link to="" className="btn btn-primary">
-								Show profile
-							</Link>
-						</div>
-					</div>
-				</li>
-			</ul>
+						</th>
+						<td>Mark</td>
+						<td>Leader</td>
+						<td>Professional</td>
+						<td>37</td>
+						<td>Male</td>
+						<td>184</td>
+					</tr>
+					<tr>
+						<th scope="row">
+							<img
+								src="images/dummy.jpg"
+								alt=""
+								width="100"
+								style={{ borderRadius: '50%' }}
+							/>
+						</th>
+						<td>Mark</td>
+						<td>Leader</td>
+						<td>Professional</td>
+						<td>37</td>
+						<td>Male</td>
+						<td>184</td>
+					</tr>
+					<tr>
+						<th scope="row">
+							<img
+								src="images/dummy.jpg"
+								alt=""
+								width="100"
+								style={{ borderRadius: '50%' }}
+							/>
+						</th>
+						<td>Mark</td>
+						<td>Leader</td>
+						<td>Professional</td>
+						<td>37</td>
+						<td>Male</td>
+						<td>184</td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
 	);
+}
+
+{
+	/* <ul>
+<li>
+	<div className="result-item">
+		<div className="card-body">
+			<h5 className="card-title">Dummy person</h5>
+			<img
+				src="images/dummy.jpg"
+				alt=""
+				width="100"
+				style={{ borderRadius: '50%' }}
+			/>
+			<p className="card-text">
+				With supporting text below as a natural lead-in to additional
+				content.
+			</p>
+			<Link to="" className="btn btn-primary">
+				Show profile
+			</Link>
+		</div>
+	</div>
+</li>
+</ul> */
 }
