b7ca92b9e993953d8dacf310a92b0b5ed345be8a
DevJ5 <36604614+DevJ5@users.noreply.github.com>
2018-11-09 19:14:46 +0100

    Fixed profile page

--- src/components/profilePage/ProfilePageContainer.js
+++ src/components/profilePage/ProfilePageContainer.js
@@ -1,28 +1,29 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import ProfilePage from "./ProfilePage";
-import { getOneProfile } from "../../actions/results";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import ProfilePage from './ProfilePage';
+import { getOneProfile } from '../../actions/results';
 
 class ProfilePageContainer extends Component {
-    componentDidMount() {
-    this.props.getOneProfile()
-  }
-  render() {
-    return (
-      <div>
-        <ProfilePage profile={this.props.profile} />
-      </div>
-    );
-  }
+	componentDidMount() {
+		this.props.getOneProfile(this.props.match.params.id);
+	}
+	render() {
+		if (!this.props.profile) return null;
+		return (
+			<div>
+				<ProfilePage profile={this.props.profile} />
+			</div>
+		);
+	}
 }
 const mapStateToProps = function(state) {
-  return {
-    login: state.login,
-    currentUser: state.currentUser,
-    profile: state.profile
-  };
+	return {
+		login: state.login,
+		currentUser: state.currentUser,
+		profile: state.profile
+	};
 };
 export default connect(
-  mapStateToProps,
-  { getOneProfile }
+	mapStateToProps,
+	{ getOneProfile }
 )(ProfilePageContainer);
