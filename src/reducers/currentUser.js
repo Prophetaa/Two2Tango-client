a5287c8cd1f9f5c3c004fbb621129cbe52403eba
DevJ5 <36604614+DevJ5@users.noreply.github.com>
2018-11-09 12:16:24 +0100

    Fixed id in state

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -1,27 +1,26 @@
-import {USER_LOGIN_SUCCESS, USER_LOGOUT} from '../actions/users'
-import { localStorageJwtKey } from '../constants'
+import { USER_LOGIN_SUCCESS, USER_LOGOUT } from '../actions/users';
+import { localStorageJwtKey } from '../constants';
 
-let initialState = null
+let initialState = null;
 try {
-  const jwt = localStorage.getItem(localStorageJwtKey)
-  if (jwt) {
-    initialState = { jwt }
-  }
-}
-catch (e) {
-  console.log(`Couldn't get data from the current State`, e)
+	const jwt = localStorage.getItem(localStorageJwtKey);
+	const id = localStorage.getItem('id');
+	if (jwt) {
+		initialState = { jwt, id };
+	}
+} catch (e) {
+	console.log(`Couldn't get data from the current State`, e);
 }
 
-export default function (state = initialState, {type, payload} ) {
+export default function(state = initialState, { type, payload }) {
 	switch (type) {
 		case USER_LOGIN_SUCCESS:
-			return payload
+			return payload;
 
-    case USER_LOGOUT:
-      return null
+		case USER_LOGOUT:
+			return null;
 
 		default:
-      return state
-    }
+			return state;
+	}
 }
-
