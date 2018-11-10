f002151c10330a294f70a594a2977bff5de13835
Ale Breso <psbreso@gmail.com>
2018-11-10 15:14:59 +0100

    navbar 40%

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,13 +1,79 @@
 import React, { Component, Fragment } from 'react';
 import { Nav, NavItem } from 'react-bootstrap';
 import { connect } from 'react-redux';
-import '../../styling/NavBar.css';
+//import '../../styling/NavBar.css';
+import logoWhite from '../../styling/images/logo-grey.png'
 
 class Navbar extends Component {
 	render() {
 		return (
 			<div>
-				<Nav id="main-nav">
+				<nav class="navbar navbar-expand-sm navbar-light bg-light sticky-top">
+					<div className="container">
+						<img className="" src={logoWhite} alt="logo"/>
+						<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+							<span className="navbar-toggler-icon"></span>
+						</button>
+						<div className="collapse navbar-collapse" id="navbarResponsive">
+						{/* <ul className="navbar-nav ml-auto d-inline-md">
+							<li className="nav-item ">
+								<button className="btn btn-outline-secondary btn-lg icon">
+									<i className="fas fa-user-circle" />
+								</button>
+							</li>
+							<li className="nav-item">
+								<i className="far fa-envelope" />
+							</li>
+							<li className="nav-item">
+								<i class="fas fa-cog"></i>
+							</li>
+							<li className="nav-item">
+								<i className="fas fa-sliders-h" />
+							</li>
+						</ul> */}
+							<div className="btn-group btn-group-toggle nav-item navbar-nav ml-auto d-inline-md" data-toggle="buttons">
+								<label className="btn btn-secondary active">
+									<input type="radio" name="options" 
+										id="option1" autocomplete="off" checked
+									/> 
+									<i className="fas fa-user-circle" />
+								</label>
+								<label className="btn btn-secondary">
+									<input type="radio" name="options" 
+										id="option2" autocomplete="off"
+									/> 
+									<i className="far fa-envelope" />
+								</label>
+								<label className="btn btn-secondary">
+									<input type="radio" name="options" 
+										id="option3" autocomplete="off"
+									/> 
+									<i class="fas fa-cog"></i>
+								</label>
+								<label className="btn btn-secondary">
+									<input type="radio" name="options" 
+										id="option3" autocomplete="off"
+									/> 
+									<i className="fas fa-sliders-h" />
+								</label>
+							</div>
+						</div>
+					</div>
+				</nav>
+			</div>
+		);
+	}
+}
+
+const mapStateToProps = function(state) {
+	return {
+		currentUser: state.currentUser
+	};
+};
+
+export default connect(mapStateToProps)(Navbar);
+
+{/* <Nav id="main-nav">
 				{!this.props.currentUser && (
 						<Fragment>
 							<NavItem className="navitem" eventKey={2} href="/login">
@@ -35,17 +101,5 @@ class Navbar extends Component {
 							</NavItem>
 						</Fragment>
 					)}
-				</Nav>
-			</div>
-		);
-	}
-}
-
-const mapStateToProps = function(state) {
-	return {
-		currentUser: state.currentUser
-	};
-};
-
-export default connect(mapStateToProps)(Navbar);
+				</Nav> */}
 
