830107521ce776e43a58b67d49fece6a4ff35356
Ale Breso <psbreso@gmail.com>
2018-11-10 23:42:51 +0100

    css

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -15,37 +15,39 @@ class Navbar extends Component {
 				<nav className="navbar navbar-expand-sm navbar-light bg-light sticky-top">
 				{ this.props.currentUser && (
 						<div className="container">
-							<img className="" src={logoWhite} alt="logo"/>
+							<Link to="/home">
+								<img className="" src={logoWhite} alt="logo"/>
+							</Link>
 							<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
 								<span className="navbar-toggler-icon"></span>
 							</button>
 							<div className="collapse navbar-collapse" id="navbarResponsive">
 								<ul className="navbar-nav ml-auto d-inline-md">
 									<li className="nav-item ">
-										<button className="btn btn-outline-secondary btn-lg icons-navbar">
+										<button className="btn btn-outline-light btn-lg icons-navbar">
 											<Link to={`/profile/${user}`} className="link-navbar">
-												<i className="fas fa-user-circle" />
+												<i className="fas fa-user-circle fa-lg" />
 											</Link>
 										</button>
 									</li>
 									<li className="nav-item">
-										<button className="btn btn-outline-secondary btn-lg icons-navbar">
+										<button className="btn btn-outline-light btn-lg icons-navbar">
 											<Link to="/messages" className="link-navbar">
-												<i className="far fa-envelope" />
+												<i className="far fa-envelope fa-lg" />
 											</Link>
 										</button>
 									</li>
 									<li className="nav-item">
-										<button className="btn btn-outline-secondary btn-lg icons-navbar">
+										<button className="btn btn-outline-light btn-lg icons-navbar">
 											<Link to="/setting" className="link-navbar">
-												<i className="fas fa-cog"></i>
+												<i className="fas fa-cog fa-lg"/>
 											</Link>
 										</button>
 									</li>
 									<li className="nav-item">
-										<button className="btn btn-outline-secondary btn-lg icons-navbar">
+										<button className="btn btn-outline-light btn-lg icons-navbar">
 											<Link to="/results" className="link-navbar">
-												<i className="fas fa-search"></i>
+												<i className="fas fa-search fa-lg"/>
 											</Link>
 										</button>
 									</li>
@@ -55,23 +57,25 @@ class Navbar extends Component {
 					)}
 				{ !this.props.currentUser && (
 					<div className="container">
-						<img className="" src={logoWhite} alt="logo"/>
+						<Link to="/home">
+							<img className="" src={logoWhite} alt="logo"/>
+						</Link>
 						<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
 							<span className="navbar-toggler-icon"></span>
 						</button>
 						<div className="collapse navbar-collapse" id="navbarResponsive">
 							<ul className="navbar-nav ml-auto d-inline-md">
 								<li className="nav-item ">
-									<button className="btn btn-outline-secondary btn-lg icons-navbar">
+									<button className="btn btn-outline-light btn-lg icons-navbar">
 										<Link to="/signup" className="link-navbar">
-											<i className="fas fa-user-plus"></i>
+											<i className="fas fa-user-plus fa-lg"/>
 										</Link>
 									</button>
 								</li>
 								<li className="nav-item">
-									<button className="btn btn-outline-secondary btn-lg icons-navbar">
+									<button className="btn btn-outline-light btn-lg icons-navbar">
 										<Link to="/login" className="link-navbar">
-											<i className="fas fa-key"></i>
+											<i className="fas fa-key fa-lg"/>
 										</Link>
 									</button>
 								</li>
