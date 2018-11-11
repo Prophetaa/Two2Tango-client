3714bd8cf6bbe1d665fde3a4ff03fda2c1d6a8e4
DevJ5 <36604614+DevJ5@users.noreply.github.com>
2018-11-11 14:29:06 +0100

    fixed homepage styling

--- src/components/homePage/Footer.js
+++ src/components/homePage/Footer.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react'
-import logoWhite from '../../styling/images/logo-grey.png'
+import React, { Component } from 'react';
+import logoWhite from '../../styling/images/logo-grey.png';
 
 export default class Footer extends Component {
-  render() {
-    return (
-      <div className="container footer-home">
-				<div className="row">
-					<div className="col-12">
-						<img src={logoWhite} alt="logo" />
+	render() {
+		return (
+			<footer>
+				<div className="container footer-home">
+					<div className="row">
+						<div className="col-12">
+							<img src={logoWhite} alt="logo" />
+						</div>
 					</div>
 				</div>
-			</div>
-    )
-  }
+			</footer>
+		);
+	}
 }
