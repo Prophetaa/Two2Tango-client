036533067c873df6e0a64842c57d02e02b004b66
Ale Breso <psbreso@gmail.com>
2018-11-12 17:22:08 +0100

    navbar

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -4,22 +4,48 @@ import { Link } from 'react-router-dom';
 
 import { fetchPreferences } from '../../actions/registration';
 import '../../styling/NavBar.css';
-import logoWhite from '../../styling/images/logo-grey.png';
+import logoWhite from '../../styling/images/logo.png';
+import partners from '../../styling/images/PARTNERS.png';
+import email from '../../styling/images/EMAIL.png';
+import user from '../../styling/images/USER.png';
+import logoXS from '../../styling/images/logoXS.png';
 
 class Navbar extends Component {
   render() {
     return (
       <div className="navbar-style">
-        <nav className="navbar navbar-expand-sm navbar-light bg-light sticky-top">
+        <nav className="navbar navbar-expand navbar-light sticky-top">
           {this.props.currentUser && (
             <div className="container">
               <div className="nav-header">
                 <Link to="/home">
-                  <img className="tangoLogo img-responsive" src={logoWhite} alt="logo" />
+                  <img className="tangoLogo" src={logoWhite} alt="logo" />
+                  <img className="tangoLogoXs " src={logoXS} alt="logo" />
                 </Link>
               </div>
               <div className="col-xs-12" id="navbarResponsive">
                 <ul className="navbar-nav ml-auto d-inline-md">
+                  <li className="nav-item">
+                    <button className="btn btn-outline-light btn-lg icons-navbar">
+                      <Link to="/messages" className="link-navbar">
+                        <img className="nav-btn" alt="icon" src={email} />
+                      </Link>
+                    </button>
+                  </li>
+                  {/* <li className="nav-item">
+                    <button className="btn btn-outline-light btn-lg icons-navbar">
+                      <Link to="/preferences" className="link-navbar">
+                        <i className="fas fa-cog fa-md" />
+                      </Link>
+                    </button>
+                  </li> */}
+                  <li className="nav-item">
+                    <button className="btn btn-outline-light btn-lg icons-navbar">
+                      <Link to="/results" className="link-navbar">
+                        <img className="nav-btn" alt="icon" src={partners} />
+                      </Link>
+                    </button>
+                  </li>
                   <li className="dropdown nav-item">
                     <div
                       className="btn-lg icons-navbar dropdown-toggle"
@@ -27,12 +53,17 @@ class Navbar extends Component {
                       role="button"
                       aria-haspopup="true"
                       aria-expanded="false">
-                      <i className="fas fa-user-circle fa-md" />
+                      <img className="nav-btn" alt="icon" src={user} />
                     </div>
                     <ul className="dropdown-menu">
                       <Link className="Links" to={'/profiles/my-profile'}>
                         <li className="dropDownLi">
                           <span className="dropDownLink">My Profile</span>
+                        </li>
+                      </Link>
+                      <Link className="Links" to={'/preferences'}>
+                        <li className="dropDownLi">
+                          <span className=" dropDownLink">Preferences</span>
                         </li>
                       </Link>
                       <Link className="Links" to={'/logout'}>
@@ -41,27 +72,6 @@ class Navbar extends Component {
                         </li>
                       </Link>
                     </ul>
-                  </li>
-                  <li className="nav-item">
-                    <button className="btn btn-outline-light btn-lg icons-navbar">
-                      <Link to="/messages" className="link-navbar">
-                        <i className="far fa-envelope fa-md" />
-                      </Link>
-                    </button>
-                  </li>
-                  <li className="nav-item">
-                    <button className="btn btn-outline-light btn-lg icons-navbar">
-                      <Link to="/preferences" className="link-navbar">
-                        <i className="fas fa-cog fa-md" />
-                      </Link>
-                    </button>
-                  </li>
-                  <li className="nav-item">
-                    <button className="btn btn-outline-light btn-lg icons-navbar">
-                      <Link to="/results" className="link-navbar">
-                        <i className="fas fa-search fa-md" />
-                      </Link>
-                    </button>
                   </li>
                 </ul>
               </div>
