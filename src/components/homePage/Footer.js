2f9c0af93a4e8847d33cbba3dbd5fc5652638238
Ale Breso <psbreso@gmail.com>
2018-11-13 14:49:38 +0100

    footer fix

--- src/components/homePage/Footer.js
+++ src/components/homePage/Footer.js
@@ -13,7 +13,6 @@ export default class Footer extends Component {
 					<div className="row">
 						<div className="col mt-md-0 mt-3">
 							<h5 className="text-uppercase">NewsLetter</h5>
-<<<<<<< HEAD
           		<p>
 							<form class="input-group">
             		<input type="text" class="form-control form-control-sm" placeholder="Your email" aria-label="Your email" aria-describedby="basic-addon2" />
@@ -36,27 +35,6 @@ export default class Footer extends Component {
             	</ul>
           	</div>
           	<div className="col-md-3 mb-md-0 mb-3">
-=======
-							<p />
-						</div>
-						<hr className="clearfix w-100 d-md-none pb-3" />
-						<div className="col mb-md-0 mb-3">
-							<h6 className="text-uppercase">Contacts</h6>
-							<ul className="list-unstyled">
-								<li>
-									<Link className="link-footer" to="#!">
-										Two2Tango@email.com
-									</Link>
-								</li>
-								<li>
-									<Link className="link-footer" to="#!">
-										cinziadmin@email.com
-									</Link>
-								</li>
-							</ul>
-						</div>
-						<div className="col-md-3 mb-md-0 mb-3">
->>>>>>> e3633329ecfeffa37fa2b61cf6786b9a7e13ef38
 							<h6 className="text-uppercase">Social</h6>
 							<ul className="list-unstyled">
 								<li className="list-footer">
