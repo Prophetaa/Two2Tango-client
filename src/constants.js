0fa75a2756965385c74272ca9b28db968fea99dd
DevJ5 <36604614+DevJ5@users.noreply.github.com>
2018-11-13 08:50:56 +0100

    Fix

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const baseUrl = process.env.REACT_APP_ENV === 'production' ? 'https://ebay-for-tickets-api.herokuapp.com' : 'http://localhost:4000'
+export const baseUrl = process.env.REACT_APP_API_URL || 'http://localhost:4000';
 
-export const localStorageJwtKey = 'currentUserJwt'
+export const localStorageJwtKey = 'currentUserJwt';
