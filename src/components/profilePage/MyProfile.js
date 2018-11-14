318ee04ffb40558c5e574f71d13cecd5c3dabd28
Tomas M <t.makauskas@gmail.com>
2018-11-14 23:58:43 +0100

    My profile page updated, sticky footer fixed for bigger screens

--- src/components/profilePage/MyProfile.js
+++ src/components/profilePage/MyProfile.js
@@ -11,45 +11,45 @@ export default function ProfilePage(props) {
 					<h1>My profile</h1>
 				</div>
 			</header>
-			<div className="container">
+			<div className="container my-profile-main">
 				<div className="row">
 					<div className="col-sm-12 col-md-4">
 						<img src={profile.photoUrl} alt="" />
 					</div>
 					<div className="col-sm-12 col-md-4 profile-details">
 						<h3 className="pb-4">Personalia</h3>
-						<div className="row">
+						<div className="row prf">
 							<div className="col-6 col-md-5 text-uppercase mb-3">Name:</div>
 							<div className="col-6 col-md-6">{profile.firstName}</div>
 						</div>
-						<div className="row">
+						<div className="row prf">
 							<div className="col-6 col-md-5 text-uppercase mb-3">City:</div>
 							<div className="col-6 col-md-6">{profile.city}</div>
 						</div>
-						<div className="row">
+						<div className="row prf">
 							<div className="col-6 col-md-5 text-uppercase mb-3">Role:</div>
 							<div className="col-6 col-md-6">{profile.role}</div>
 						</div>
-						<div className="row">
+						<div className="row prf">
 							<div className="col-6 col-md-5 text-uppercase mb-3">Level:</div>
 							<div className="col-6 col-md-6">{profile.level}</div>
 						</div>
-						<div className="row">
+						<div className="row prf">
 							<div className="col-6 col-md-5 text-uppercase mb-3">Height:</div>
 							<div className="col-6 col-md-6">{profile.height} cm</div>
 						</div>
-						<div className="row">
+						<div className="row prf">
 							<div className="col-6 col-md-5 text-uppercase mb-3">Age</div>
 							<div className="col-6 col-md-6">{profile.age}</div>
 						</div>
-						<div className="row">
+						<div className="row prf">
 							<div className="col-6 col-md-5 text-uppercase mb-3">Gender:</div>
 							<div className="col-6 col-md-6">{profile.gender}</div>
 						</div>
 					</div>
 					<div className="col-sm-12 col-md-4">
-						<h3 className="pb-4">About me:</h3>
-						<p>{profile.about}</p>
+						<h3 className="pb-4">About me</h3>
+						<p className='about'>{profile.about}</p>
 
 						<button
 							onClick={() => history.push('/edit-profile')}
