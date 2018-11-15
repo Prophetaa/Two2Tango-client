2c4272a7b178e035a260b8bb235c9cd0adfcc495
Prophetaa <klaux_15@hotmail.com>
2018-11-15 16:02:49 +0100

    done

--- src/constants.js
+++ src/constants.js
@@ -6,5 +6,5 @@ export const localStorageJwtKey = 'currentUserJwt';
 export const CLOUDINARY_UPLOAD_PRESET = 'ieb90awf';
 export const CLOUDINARY_UPLOAD_URL ='https://api.cloudinary.com/v1_1/dg6hu5lub/image/upload'
 
-export const GOOGLE_API_KEY = '6LepOXoUAAAAAHN3GTNxdcI4rZ_96K118NiJpAIj'
+export const GOOGLE_API_KEY = '6LeV1HoUAAAAAJecLRoYy7CKZSBvi_8D03bmqhzQ'
 export const FACEBOOK_API_KEY = '516292132220329'
